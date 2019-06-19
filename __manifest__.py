ae4afd77fff8cdbd1ece6d4c4318959c7bd9a50a
Maxim Litnitskiy <litnimaxster@gmail.com>
2019-06-19 09:10:20 +0300

    Category fix

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     'maintainer': 'Odooist',
     'support': 'odooist@gmail.com',
     'license': 'LGPL-3',
-    'category': 'Hidden',
+    'category': 'Tools',
     'summary': 'Example of custom remote agent',
     'description': "",
     'website': 'http://github.com/litnimax/remote_agent_example',
